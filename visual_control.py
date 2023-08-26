a3221ab0fb8143135f55e4dbebcc74d91ea3b00b
Sadeep Ariyarathna <52843991+elemenceOR@users.noreply.github.com>
2023-08-26 18:07:46 +0200

    Update visual_control.py

--- visual_control.py
+++ visual_control.py
@@ -4,8 +4,6 @@ from time import sleep
 from luma.core.interface.serial import spi, noop
 from luma.led_matrix.device import max7219
 from luma.core.render import canvas
-from main import e, f, g, temp_buffer, hum_buffer
-
 
 ADDRESS = 0x70
 
@@ -22,17 +20,19 @@ device = max7219(serial_interface, cascaded=1, block_orientation=90, rotate=0)
 
 display = SevenSegment.SevenSegment(address=ADDRESS)
 
-measurements = [e, f, g, temp_buffer, hum_buffer]
+# data_field_1 (aqi1.0)
+# data_field_2 (aqi2.5)
+# data_field_3 (aqi10.0)
+# need a method to get these arraz from the get_from_cloud() function ?
+measurements = [data_field_1, data_field_2, data_field_3, temp_buffer, hum_buffer]
 total_measurements = len(measurements)
 
 selected_measurement = 0
-
 
 def update_display():
     with canvas(device) as draw:
         for i in range(total_measurements):
             draw.point((i, 7), fill="white" if i == selected_measurement else "black")
-
 
 while True:
     display.begin()
